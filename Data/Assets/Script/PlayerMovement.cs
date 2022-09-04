a0778ed036385df02716bdf742057aa178734f12
alwan01 <yahyaalwan0401@yahoo.de>
2022-09-04 12:08:53 +0200

    first commit

---
+++ Data/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+    
+    void FixedUpdate()
+    {
+        rb.AddForce(forwardForce * Time.deltaTime, 0, 0);
+
+        if ( Input.GetKey("d"))
+        {
+            rb.AddForce(0, 0, - sidewayForce * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(0, 0, sidewayForce * Time.deltaTime);
+        }
+
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
